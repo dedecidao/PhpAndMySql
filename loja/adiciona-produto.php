6909d0c2274aafca0ac01e6741b137498b5d07bf
André Luis Araujo <andrearaujoti@outlook.com>
2019-04-30 16:51:32 -0300

    Primeiro Commit
    Uma pagina que adiciona produto e insere no banco de dados.

---
+++ loja/adiciona-produto.php
@@ -0,0 +1,20 @@
+﻿
+<?php include("cabecalho.php");
+
+$nome = $_GET["nome"];
+$preco = $_GET["preco"];
+
+$query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+$conexao = mysqli_connect('localhost', 'root', '', 'loja');
+
+if(mysqli_query($conexao, $query)) {
+?>
+<p class="alert-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+<?php
+} else {
+?>
+<p class="alert-danger">O produto <? = $nome; ?> não foi adicionado</p>
+<?php
+}
+
+?>
